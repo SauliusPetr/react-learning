5be0d24be24004e2c7078a6cb06e0e5e0a6c099a
Saulius <petruseviciuss@gmail.com>
2024-03-15 17:23:44 +0200

    adding weather app

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,26 +1,33 @@
-//needs a statefor location
-//button trigger will trigger the fetching of data from weather api
-
 import { useState } from "react";
 
+let defaultCountry = await initWeather("Oslo");
+
 export function WeatherApp() {
-  const [location, setLocation] = useState("");
-  const [weatherData, setWeatherData] = useState({});
+  const [location, setLocation] = useState("Sweden");
+  const [weatherData, setWeatherData] = useState(defaultCountry);
 
   async function handleWeatherRequest(location) {
     let response = await requesWeatherData(location);
-    setWeatherData(response);
-    //while thedata is loading make a spinner
-    //then make a funiton that will display the data
+    if (Object.keys(response).length !== 0) {
+      setWeatherData(pullData(response));
+    }
   }
 
   return (
-    <div>
+    <div className="weather-app-container">
+      <div className="weather-container">
+        <div>
+          <h2>{weatherData.city}</h2>
+          <h5>{weatherData.country}</h5>
+        </div>
+        <img src={weatherData.icon} />
+        <div className="temperature">{weatherData.tempC + "°C"}</div>
+      </div>
       <div className="searchbar">
         <label htmlFor={"loaction"}>
-          {"Location: "}
           <input
             id="loaction"
+            placeholder="Oslo"
             onChange={(event) => {
               setLocation(event.target.value);
             }}
@@ -30,20 +37,56 @@ export function WeatherApp() {
           Submit
         </button>
       </div>
-      <div></div>
+      <div>
+        <div>{`Humidity: ${weatherData.humidity}%`}</div>
+        <div>{`Windspeed: ${weatherData.windspeed} KPH`}</div>
+        <div>{`Last Updated: ${weatherData.lastUpdated}`}</div>
+      </div>
     </div>
   );
+}
+
+function loadingAnimation() {
+  let container = document.querySelector("weather-app-container");
+  container.setAttribute("style", "background-size:cover");
+}
+
+async function initWeather(location) {
+  let response = await requesWeatherData(location);
+  return pullData(response);
+}
+
+function pullData(weatherData) {
+  return {
+    country: weatherData.location.country,
+    city: weatherData.location.name,
+    tempF: weatherData.current.temp_f,
+    tempC: weatherData.current.temp_c,
+    humidity: weatherData.current.humidity,
+    windspeed: weatherData.current.wind_kph,
+    icon: weatherData.current.condition.icon,
+    condition: weatherData.current.condition.text,
+    lastUpdated: weatherData.current.last_updated,
+  };
 }
 
 async function requesWeatherData(location) {
   let apiKey = "c4332bbbf71b419ab07121955241403";
   let url = "https://api.weatherapi.com/v1/current.json";
   let fetchUrl = `${url}?key=${apiKey}&q=${location}`;
+  let weatherData = {};
   try {
-    let response = await fetch(fetchUrl, { mode: "cors" });
-    let weatherData = await response.json();
+    let response = await fetch(fetchUrl, { mode: "cors" }).catch((err) =>
+      console.log("aaa")
+    );
+    if (!response.ok) {
+      return {};
+    }
+
+    weatherData = await response.json();
+
     return weatherData;
   } catch (error) {
-    console.log("Error: " + error);
+    console.log("Error: ", error);
   }
 }
